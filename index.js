ac55dbe3281429453b03f1e4fab2e7c2bee729d5
odevbruno <bruno.oliveira@externos-br.cencosud.com>
2024-03-06 23:51:02 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,184 @@
+require('dotenv').config();
+const stream = require("./stream");
+const axios = require('axios');
+const { Telegraf } = require('telegraf');
+const Binance = require('binance-api-node').default;
+const { log, info, error } = require('console')
+
+const QUOTE = process.env.QUOTE;
+const PROFITABILITY = parseFloat(process.env.PROFITABILITY);
+const AMOUNT = parseFloat(process.env.AMOUNT);
+const CRAWLER_INTERVAL = parseInt(process.env.CRAWLER_INTERVAL);
+const bot = new Telegraf(process.env.TELEGRAM_API_BOT);
+
+// const client = Binance({
+//   apiKey: process.env.API_KEY,
+//   apiSecret: process.env.SECRET_KEY,
+// });
+
+async function exchangeInfo() {
+  const response = await axios.get("https://api.binance.com/api/v3/exchangeInfo");
+  return response.data.symbols.filter(s => s.status === 'TRADING').map(s => {
+    return {
+      symbol: s.symbol,
+      base: s.baseAsset,
+      quote: s.quoteAsset
+    }
+  });
+}
+
+function getBuyBuySell(buySymbols, allSymbols, symbolsMap) {
+  const buyBuySell = [];
+
+  for (let i = 0; i < buySymbols.length; i++) {
+    const buy1 = buySymbols[i];
+
+    const right = allSymbols.filter(s => s.quote === buy1.base);
+
+    for (let j = 0; j < right.length; j++) {
+      const buy2 = right[j];
+
+      const sell1 = symbolsMap[buy2.base + buy1.quote];
+      if (!sell1) continue;
+
+      buyBuySell.push({ buy1, buy2, sell1 });
+    }
+  }
+  return buyBuySell;
+}
+
+function getBuySellSell(buySymbols, allSymbols, symbolsMap) {
+  const buySellSell = [];
+  for (let i = 0; i < buySymbols.length; i++) {
+    const buy1 = buySymbols[i];
+
+    const right = allSymbols.filter(s => s.base === buy1.base && s.quote !== buy1.quote);
+
+    for (let j = 0; j < right.length; j++) {
+      const sell1 = right[j];
+
+      const sell2 = symbolsMap[sell1.quote + buy1.quote];
+      if (!sell2) continue;
+
+      buySellSell.push({ buy1, sell1, sell2 });
+    }
+  }
+  return buySellSell;
+}
+
+function getSymbolMap(symbols) {
+  const map = {};
+  symbols.map(s => map[s.symbol] = s);
+  return map;
+}
+
+async function processBuyBuySell(buyBuySell) {
+  for (let i = 0; i < buyBuySell.length; i++) {
+    const candidate = buyBuySell[i];
+
+    //verifica se já temos todos os preços
+    let priceBuy1 = stream.getBook(candidate.buy1.symbol);
+    if (!priceBuy1) continue;
+    priceBuy1 = parseFloat(priceBuy1.price);
+
+    let priceBuy2 = stream.getBook(candidate.buy2.symbol);
+    if (!priceBuy2) continue;
+    priceBuy2 = parseFloat(priceBuy2.price);
+
+    let priceSell1 = stream.getBook(candidate.sell1.symbol);
+    if (!priceSell1) continue;
+
+    priceSell1 = parseFloat(priceSell1.price);
+
+    //se tem o preço dos 3, pode analisar a lucratividade
+    const crossRate = (1 / priceBuy1) * (1 / priceBuy2) * priceSell1;
+    if (crossRate > PROFITABILITY) {
+      const MSG1 = `OP BBS EM ${candidate.buy1.symbol} > ${candidate.buy2.symbol} > ${candidate.sell1.symbol} = ${crossRate}`;
+      const MSG2 = `Investindo ${QUOTE}${AMOUNT}, retorna ${QUOTE}${((AMOUNT / priceBuy1) / priceBuy2) * priceSell1}`;
+      console.log(MSG1);
+      console.log(MSG2);
+      bot.telegram.sendMessage(process.env.CHAT_ID, `
+      BUY BUY SELL - ${candidate.buy1.symbol} > ${candidate.buy2.symbol} > ${candidate.sell1.symbol}
+      INVEST: ${AMOUNT} - ${QUOTE}
+      RETURN: ${((AMOUNT / priceBuy1) / priceBuy2) * priceSell1} - ${QUOTE}`);
+    }
+  }
+}
+
+function processBuySellSell(buySellSell) {
+  for (let i = 0; i < buySellSell.length; i++) {
+    const candidate = buySellSell[i];
+
+    //verifica se já temos todos os preços
+    let priceBuy1 = stream.getBook(candidate.buy1.symbol);
+    if (!priceBuy1) continue;
+    priceBuy1 = parseFloat(priceBuy1.price);
+
+    let priceSell1 = stream.getBook(candidate.sell1.symbol);
+    if (!priceSell1) continue;
+    priceSell1 = parseFloat(priceSell1.price);
+
+    let priceSell2 = stream.getBook(candidate.sell2.symbol);
+    if (!priceSell2) continue;
+    priceSell2 = parseFloat(priceSell2.price);
+
+    //se tem o preço dos 3, pode analisar a lucratividade
+    const crossRate = (1 / priceBuy1) * priceSell1 * priceSell2;
+    if (crossRate > PROFITABILITY) {
+      const MSG1 = `OP BSS EM ${candidate.buy1.symbol} > ${candidate.sell1.symbol} > ${candidate.sell2.symbol} = ${crossRate}`;
+      const MSG2 = `Investindo ${QUOTE}${AMOUNT}, retorna ${QUOTE}${((AMOUNT / priceBuy1) * priceSell1) * priceSell2}`;
+      console.log(MSG1);
+      console.log(MSG2);
+
+      bot.telegram.sendMessage(process.env.CHAT_ID, `
+      BUY SELL SELL - ${candidate.buy1.symbol} > ${candidate.sell1.symbol} > ${candidate.sell2.symbol}
+      INVEST: ${QUOTE}${AMOUNT}
+      RETURN: ${QUOTE}${((AMOUNT / priceBuy1) * priceSell1) * priceSell2}
+      `);
+
+    }
+  }
+}
+
+async function start() {
+  // const balance = await client.accountInfo();
+  // info('Seu saldo em ETH é: ', balance.balances.filter(f => f.asset === 'ETH').map(b => [b.asset] = b.free));
+  // info('Creating order binance test...');
+  // const data = {
+  //   symbol: 'BTCUSDT',
+  //   side: 'BUY',
+  //   type: 'MARKET',
+  //   quantity: "0.0001",
+  //   timestamp: Date.now(),
+  //   recvWindow: 60000,
+  // };
+
+  // const resOrder = await client
+  //   .order(data);
+
+  //pega todas moedas que estão sendo negociadas
+  log('Loading Exchange Info...');
+  const allSymbols = await exchangeInfo();
+
+  //moedas que você pode comprar
+  const buySymbols = allSymbols.filter(s => s.quote === QUOTE);
+  log('There are ' + buySymbols.length + " pairs that you can buy with " + QUOTE);
+
+  //organiza em map para performance
+  const symbolsMap = getSymbolMap(allSymbols);
+
+  //descobre todos os pares que podem triangular BUY-BUY-SELL
+  const buyBuySell = getBuyBuySell(buySymbols, allSymbols, symbolsMap);
+  log('There are ' + buyBuySell.length + " pairs that we can do BBS");
+
+  //descobre todos os pares que podem triangular BUY-SELL-SELL
+  const buySellSell = getBuySellSell(buySymbols, allSymbols, symbolsMap);
+  log('There are ' + buySellSell.length + " pairs that we can do BSS");
+
+  setInterval(async () => {
+    log(new Date());
+    processBuyBuySell(buyBuySell);
+    processBuySellSell(buySellSell);
+  }, CRAWLER_INTERVAL)
+}
+start();
---
+++ stream.js
@@ -0,0 +1,18 @@
+
+const WebSocket = require('ws');
+const ws = new WebSocket("wss://stream.binance.com:9443/ws/!miniTicker@arr");
+
+const BOOK = {};
+
+ws.onmessage = async (event) => {
+  const arr = JSON.parse(event.data);
+  arr.map(obj => BOOK[obj.s] = { price: parseFloat(obj.c) })
+}
+
+function getBook(symbol) {
+  return BOOK[symbol];
+}
+
+module.exports = {
+  getBook
+}
